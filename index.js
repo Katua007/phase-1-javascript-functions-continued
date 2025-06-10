7c3a4cc35901016c448074d647aed3dbec57361a
katua <katuacyril@gmail.com>
2025-06-10 12:36:32 +0300

    JavaScript Lab

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // code your solution here
+function saturdayFun(activity) {
+    if (activity === undefined) {
+        activity = "roller-skate";
+    }
+    return `This Saturday, I want to ${activity}!`;
+}
+
+function mondayWork(activity) {
+    if (activity === undefined) {
+        activity = "go to the office";
+    }
+    return `This Monday, I will ${activity}.`;
+}
+
+function wrapAdjective(flair = "*") {
+    if (typeof flair !== 'string') {
+        throw new Error("Flair must be a string");
+    }
+    return function(adjective) {
+        return `You are ${flair}${adjective}${flair}!`;
+    };
+}
+function Calculator() {
+  this.add = function(a, b) {
+    return a + b;
+  };
+  this.subtract = function(a, b) {
+    return a - b;
+  };
+  this.multiply = function(a, b) {
+    return a * b;
+  };
+  this.divide = function(a, b) {
+    return a / b;
+  };
+}
